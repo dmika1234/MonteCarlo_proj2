8e3b982d6ad015dbc3957a4a170fd73397c00615
Dominik <dmika1234@gmail.com>
2022-06-15 14:45:12 +0200

    addede CMC for n>1

--- estimator_funs.py
+++ estimator_funs.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.stats import norm
 from scipy.stats.distributions import chi2
-from functools import partial
+
 
 # Exact formula used for case: n=1
 def eu_exact(K, r, s0, sigma):
@@ -18,11 +18,42 @@ def gbm(x, s0, r, sigma):
     return st
 
 
+# Creating covariance matrix
+def create_sigma(n):
+    sigma = np.full((n, n), 0)
+    for i in np.arange(n):
+        for j in np.arange(n):
+            sigma[i, j] = np.minimum(i, j) + 1
+
+    sigma = sigma / n
+    return sigma
+
+
 # CMC estimator
+#n=1
 def eu_option_cmc(K, r, s0, sigma, R, seed=2022):
     np.random.seed(seed)
     x = np.random.normal(0, 1, R)
     An = gbm(x, s0, r, sigma)
+    Yk = np.maximum(An - K, 0)
+    Yhat = np.mean(Yk) * np.exp(-r)
+    return Yhat
+
+
+# general n
+def eu_option_cmc_n(K, r, s0, sigma, R, n, seed=2022):
+    np.random.seed(seed)
+    u = np.random.random(int(R))
+    D = np.sqrt(chi2.ppf(u, df=n))
+    ksi = np.random.multivariate_normal(mean=np.zeros(n), cov=np.identity(n), size=int(R))
+    ksi_len = np.sqrt(np.sum(ksi ** 2, axis=1))
+    X = (ksi.T / ksi_len).T
+    Z = (X.T * D).T
+    sigma_matrix = create_sigma(n)
+    A = np.linalg.cholesky(sigma_matrix)
+    B = np.dot(A, Z.T).T
+    St = gbm(B, s0, r, sigma)
+    An = np.sum(St, axis=1) / n
     Yk = np.maximum(An - K, 0)
     Yhat = np.mean(Yk) * np.exp(-r)
     return Yhat
@@ -69,16 +100,6 @@ def eu_option_cv(K, r, s0, sigma, R, seed=2022):
 
 
 # Stratified estimator
-def create_sigma(n):
-    sigma = np.full((n, n), 0)
-    for i in np.arange(n):
-        for j in np.arange(n):
-            sigma[i, j] = np.minimum(i, j) + 1
-
-    sigma = sigma / n
-    return sigma
-
-
 def eu_option_strat_prop(K, r, s0, sigma, R, nr_of_strata=4, n=1, seed=2022):
     if R % nr_of_strata != 0:
         raise ValueError('Value of R must be dividable by nr_of_strata!')
